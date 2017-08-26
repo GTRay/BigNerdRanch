a0a5cff37d6c7deec2bcbfe6b93dc6eb4f1ad628
Rayleigh <ruizhichai@gmail.com>
2017-08-26 16:51:14 -0400

    drag draw

---
+++ DragAndDraw/app/src/main/java/com/rchai3/draganddraw/Box.java
@@ -0,0 +1,29 @@
+package com.rchai3.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by rayleigh on 8/26/17.
+ */
+
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
---
+++ DragAndDraw/app/src/main/java/com/rchai3/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,24 @@
+package com.rchai3.draganddraw;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rayleigh on 8/26/17.
+ */
+
+public class DragAndDrawFragment extends Fragment {
+
+    public static DragAndDrawFragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+        return v;
+    }
+}
